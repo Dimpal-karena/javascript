c69f32e286c002317678fe33e51b8b3e0c1cc35b
Dimpal karena <dimpalkarena2@gmail.com>
2025-11-11 17:58:19 +0530

    practise

---
+++ first.js
@@ -0,0 +1,33 @@
+(function () {
+  "use strict";
+  /* Start of your code */
+  function greetMe(yourName) {
+    alert(`Hello ${yourName}`);
+  }
+
+  greetMe("World");
+  /* End of your code */
+})();
+
+
+Math.ceil(2.1);
+Math.floor(2.9);
+Math.round(2.5);
+Math.max(1, 3, 2);
+Math.min(1, 3, 2);
+Math.random();
+Math.sqrt(16);
+Math.pow(2, 3);
+Math.abs(-5);
+Math.log(10);
+Math.exp(1);
+Math.sin(Math.PI / 2);
+Math.cos(0);
+Math.tan(Math.PI / 4);
+Math.asin(1);
+Math.acos(0);
+Math.atan(1);
+Math.atan2(1, 1);
+Math.cbrt(27);
+Math.clz32(1);
+Math.fround(1.5);
