9939df5a9e5ee0642e9f7df59c1624f13e1b342e
Dimpal karena <dimpalkarena2@gmail.com>
2025-11-13 22:38:12 +0530

    number-game

---
+++ number-game.js
@@ -0,0 +1,14 @@
+const min = 1;
+const max = 100;
+const answer = Math.random();
+console.log(`answer is ${answer}`);
+
+// <!-- let attempts = 0;
+// let guess;
+// let runing = true;
+// while(running){
+
+//   guess = window.prompt(`guess a number between ${min} and ${max}:`);
+//   console.log(typeof guess,guess);
+//   running = false;
+// } -->
