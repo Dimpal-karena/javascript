f4cf8757bb236e0f97f74777dd6801a6414513cb
Dimpal karena <dimpalkarena2@gmail.com>
2025-11-16 21:25:01 +0530

    type conversion

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+
+
 document.getElementById('mysubmit').onclick = function(){
   username = document.getElementById('mytext').value;
   document.getElementById('myh1').textContent = `helloo ${username}`;
-}
+};
+
+
+let x = '';
+let y = '';
+let z = '';
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
